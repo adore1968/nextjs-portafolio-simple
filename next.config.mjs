28e55bc8fdc1304aac4f9d80b68acbb7568a8aa0
josé <jg345762@gmail.com>
2024-07-17 14:40:50 -0300

    next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "nextjs-portafolio-simple",
+  output: "export",
+};
 
 export default nextConfig;
